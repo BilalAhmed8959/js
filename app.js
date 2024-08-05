d0c9132f6cfb217619e400916a820d162da80ecd
BilalAhmed8959 <bilalahmed8959@gmail.com>
2024-08-05 04:36:10 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+
+// concatinate
+// var username = "abdullah";
+// var age = 21;
+
+// console.log("my name is " + username + " and my age is " + age)
+
+
+
+
+
+// var city = 20;
+// var rem = 3;
+
+// console.log(city % rem)
+
+// var num = 20;
+
+// num -= 10;
+// console.log(num);
+
+
+
+// < greater than
+// > less than
+// == data type  
+// ===
+// !=
+// !==
+// <=
+// >=
+
+// console.log(20 < 10 && 20 != "20")
+
+
+
+
+// var name = "bilal ahmed";
+// var age = 24;
+
+// console.log("my name " + name + "my age" + age)
+
+
