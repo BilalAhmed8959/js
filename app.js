c62275915a53b9a244b9b7acabb308b5a07a0a4a
BilalAhmed8959 <bilalahmed8959@gmail.com>
2024-08-14 08:41:10 -0700

    addd

--- app.js
+++ app.js
@@ -1,162 +1,243 @@
 
 
-// // var age = prompt("enter your age");
+var age = prompt("enter your age");
 
 
-// // if (age < 15 || age >50){
-// //     document.write("addmission nahi mily ga")
+if (age < 15 || age >50){
+    document.write("addmission nahi mily ga")
     
-// // }else if (age <= 18) {
-// //     document.write("mithai (500) do or ajao")
-// // } 
+}else if (age <= 18) {
+    document.write("mithai (500) do or ajao")
+} 
 
 
-// // var num = prompt("guess the number");
+var num = prompt("guess the number");
 
-// // if(num <=5 ){
-// //     document.write("haar gaey")
-// // }else if(num == 6 || num == 8 || num == 9 || num == 10 ){
-// //     document.write("malik qareeb qareeb ho")
-// // }else if(num > 10){
-// //     document.write("malik number 10 sy chota doo")
-// // }else if (num == 7){
-// //     document.write("malik jeet gaey")
-// // } else{
-// //     document.write("bhar me jao")
-// // }
-
-
-// // var marks = +prompt("Obten Marks");
-
-// // if( marks < 400){
-// //     document.write(marks / 400 * 100 + "%")
-// // } else{
-// //     document.write(".....")
-// // }
+if(num <=5 ){
+    document.write("haar gaey")
+}else if(num == 6 || num == 8 || num == 9 || num == 10 ){
+    document.write("malik qareeb qareeb ho")
+}else if(num > 10){
+    document.write("malik number 10 sy chota doo")
+}else if (num == 7){
+    document.write("malik jeet gaey")
+} else{
+    document.write("bhar me jao")
+}
 
 
+var marks = +prompt("Obten Marks");
 
-// // function bilal (){ 
-// //     document.write("hlo bilal")
-// // }
-// // bilal ()
+if( marks < 400){
+    document.write(marks / 400 * 100 + "%")
+} else{
+    document.write(".....")
+}
 
 
 
-// function age ( user , age1){
-//  console.log("my name is" + user , "my age is" + age1)
-// }
-
-// age("bila" , 22)
-
+function bilal (){ 
+    document.write("hlo bilal")
+}
+bilal ()
 
 
+
+function age ( user , age1){
+ console.log("my name is" + user , "my age is" + age1)
+}
+
+age("bila" , 22)
 
 
 
 
 
 
-// function addition (num1 , num2){
-//     return num1 + num2    
-// }
-
-// var result = addition(30 , 30)
-// console.log(result);
 
 
 
-// function result (chemMarks , phyMarks , comMarks , engMarks){
-//     return chemMarks + phyMarks + comMarks + engMarks / 100 * 400 
+function addition (num1 , num2){
+    return num1 + num2    
+}
 
-// }
-// var calculate = result(100,100,100,100)
-// console.log(calculate + "%");
-
-
+var result = addition(30 , 30)
+console.log(result);
 
 
 
-// function result (num1 , num2 , num3 ,num4){
-//     var totel = num1 + num2 + num3 +num4
-//     return totel /100 *400 
-// }
-// var bila = result( 50 , 40 , 50 ,30);
+function result (chemMarks , phyMarks , comMarks , engMarks){
+    return chemMarks + phyMarks + comMarks + engMarks / 100 * 400 
 
-
-// var head = document.querySelector("p");
-// console.log(head.innerHTML);
-// head.innerHTML = "<b> <fu>Ali baba 40 chor</fu
+}
+var calculate = result(100,100,100,100)
+console.log(calculate + "%");
 
 
 
-// function div (){
-//     button.innerHTML = "<h1>Abdul Hadi</h1> <br/> <ul><li>Home</li><li>About</li><li>contact</li></ul>"
-// }
+
+
+function result (num1 , num2 , num3 ,num4){
+    var totel = num1 + num2 + num3 +num4
+    return totel /100 *400 
+}
+var bila = result( 50 , 40 , 50 ,30);
+
+
+var head = document.querySelector("p");
+console.log(head.innerHTML);
+head.innerHTML = "<b> <fu>Ali baba 40 chor</fu
+
+
+
+function div (){
+    button.innerHTML = "<h1>Abdul Hadi</h1> <br/> <ul><li>Home</li><li>About</li><li>contact</li></ul>"
+}
  
-// .......................question1...............
-// function abc (){
-    //     document.write("hlo word")
-    // }
+.......................question1...............
+function abc (){
+        document.write("hlo word")
+    }
     
-    // abc ()
-    // abc ()
-    
-    
-    // .......................question2...............
-
-
-//     function dateNTime(){
-//         document.write()
-//     }
-
-// dateNTime()
-
-
-// function fristName(fristName, lastName){
-//     document.write("First name " + fristName , "last Name " + lastName);
-    
-// }
-// fristName("Bilal" , "Ahmad");
-
-
-// ...........question4....
-// function sum (num1 , num2){
-    //     return num1 + num2
-    // }
-    // var sum1 = sum (+prompt("enter num1") , +prompt("enter num2"))
-    // document.write(sum1)
+    abc ()
+    abc ()
     
     
-    // ...........question5....
+    .......................question2...............
+
+
+    function dateNTime(){
+        document.write()
+    }
+
+dateNTime()
+
+
+function fristName(fristName, lastName){
+    document.write("First name " + fristName , "last Name " + lastName);
+    
+}
+fristName("Bilal" , "Ahmad");
+
+
+...........question4....
+function sum (num1 , num2){
+        return num1 + num2
+    }
+    var sum1 = sum (+prompt("enter num1") , +prompt("enter num2"))
+    document.write(sum1)
+    
+    
+    ...........question5....
     
 
 
-    // function sum (num1 , num2 , operator){
-    //     if(num1 == "+"){
-    //         document.write(num1+num2)
-    //     }
-    // }
+    function sum (num1 , num2 , operator){
+        if(num1 == "+"){
+            document.write(num1+num2)
+        }
+    }
     
     
-    // function sum (num1 , num2 ){
-    //     document.write(num1 * num2)
+    function sum (num1 , num2 ){
+        document.write(num1 * num2)
 
-    // }
-    // sum (4 , 4)
+    }
+    sum (4 , 4)
     
-// var sum1 = document.querySelector("p")
+var sum1 = document.querySelector("p")
 
 
-// var sum = document.querySelector("#button_1")
-// if(sum == sum1){
+var sum = document.querySelector("#button_1")
+if(sum == sum1){
 
-// }
+}
 var sum =document.querySelector("button")
 
 function one (){
     document.write(sum =+ 1)
 }
+
+
+
+function sum (num1 , num2 ){
+return num1 , num2 
+}
+
+var dividedNum = prompt("Enter Number")
+
+if (dividedNum % 3 === 0){
+    document.write("divided by 3")
+}else {
+    document.write("Number is not divided by 3")
+}
+
+
+
+
+var numbr = prompt("enter you number")
+
+if( numbr % 2 === 0){
+    document.write("even")
+
+}else{
+    document.write("odd")
+}
+
+
+var age = prompt("enter your age") 
+if(age > 18){
+document.write("old enf")
+}else {
+document.write("too ymm")
+}
+
+
+
+
+
+var time = prompt('Time')
+if (time >= 0000 && time < 1200){
+    document.write("Good Morning")
+}else if( time >= 1200 && time < 1700){
+    document.write("Good Afternoon")
+}else {
+    document.write("god night")
+}
+
+
+
+
+
+
+var nubr = 20;
+if (nubr == 20){
+document.write("nubr 12 sy bra ha")
+}else{
+    document.write("nubr chta ha")
+}
+
+
+
+var age = prompt("enter your age");
+if(age <= 18){
+document.write("ap ki age 18 sy km ha ")
+}else{
+    document.write("ap ki age 18 sy zyda ha")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
